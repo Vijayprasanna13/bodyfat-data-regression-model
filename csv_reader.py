fafe4b0d6f97d068bef31df1562c7a0be174ace6
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2017-03-26 02:13:13 +0530

    Add ANN with single node

--- csv_reader.py
+++ csv_reader.py
@@ -10,10 +10,10 @@ as an object list
 """
 
 def readCSV(filename):
-	folder_path = os.path.dirname(os.path.abspath(__file__))
+	#folder_path = os.path.dirname(os.path.abspath(__file__))
 	#contains the data set after the reading of the csv file
 	dataset  = []
-	with open(folder_path+'/CSV/'+filename+'.csv','rb') as file:
+	with open(filename+'.csv','rb') as file:
 		reader = csv.reader(file)
 		for row in reader:
 			 temp = row[0].split()
@@ -29,11 +29,20 @@ def readCSV(filename):
 
 	#append features and target to object
 	for data in dataset:
-	 target = data[0]
+	 target = [data[0]]
 	 del data[0]
 	 features = []
 	 for datum in data:
 	  features.append( (datum.split(":"))[1] )
 	 bodyfat_data.append(DataPoint(target,features))
-
-	return bodyfat_data 
+	 i = 0
+	 N = len(bodyfat_data)
+	 f = open('rf','w')
+	 for datum in bodyfat_data:
+	 	f.write(datum.target[0])
+	 	f.write(',')
+	 	for tar in datum.features[:-1]:
+	 		f.write(tar)
+	 		f.write(',')
+	 	f.write(datum.features[len(datum.features)-1])
+	 	f.write('\n')
