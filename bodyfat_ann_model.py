3618af7552119e45beea660e16a6814828118aab
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2017-04-07 23:34:08 +0530

    added hidden layer with 7 neurons

--- bodyfat_ann_model.py
+++ bodyfat_ann_model.py
@@ -2,12 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-asdas
-asdasd
+def sigmoidal(w,x):
+    return 1 / (1 + np.exp(-(np.dot(np.transpose(w),x))))
+
 #Set the learning rate and No of epochs
-eta = 0.3E-3
+eta = 0.29E-1
 max_hidden_nodes = 7
-max_epochs = 40
+max_epochs = 10
 filename = 'bodyfat.csv'
 
 fid = open(filename, "r")
@@ -45,23 +46,49 @@ Y = X_file_training[:,0]
 for i in range(1,np.shape(X)[1]):
 	X[:,i] = (X[:, i]-np.mean(X[:, i]))/np.std(X[:, i])
 
-
-
-w = np.zeros(np.shape(X)[1])
-
-
 sse = []
 sse_testing_i = []
 epoch_index = []
 
+p = np.zeros([np.shape(X)[1],max_hidden_nodes])
+q = np.zeros(max_hidden_nodes)
+
+for t in range(0, max_epochs):
+	#gradients for wieghts q and p
+	grad_q = np.zeros(max_hidden_nodes)
+	grad_p = np.zeros([np.shape(X)[1],max_hidden_nodes])
+	for i in range(0,N):
+		x_i = X[i,:]
+		y_i = Y[i]
+		#compute h matrix
+		h = np.zeros(max_hidden_nodes)
+		for k in range(0, max_hidden_nodes):
+			h[k] = sigmoidal(p[:,k],x_i)
+		o_i = np.dot(np.transpose(h), q)
+		error = (y_i - o_i)
+		grad_q += 2*error*h
+		for k in range(0, np.shape(X)[1]):
+			grad_p[k] = 2*x_i[k]*error*h*(1-h)*q 
+	q = q - eta*grad_q
+	p = p - eta*grad_p
+	sse_i = 0
+	for i in range(0, N):
+		x_i = X[i, :]
+		y_i = Y[i]
+		h = np.zeros(max_hidden_nodes)
+		for k in range(0, max_hidden_nodes):
+			h[k] = sigmoidal(p[:,k],X[i,:])
+			o_i = np.dot(np.transpose(h), q)
+		sse_i += pow((o_i - y_i),2)
+	sse.append(sse_i)
 #Run for max_epochs number of epochs and update the wieght by calculating the gradient. 
 #Record the cost function output for error for each epoch
+"""
 for t in range(0, max_epochs):
 	gradient = np.zeros(np.shape(X)[1]) 
 	for i in range(0, N):
 		x_i = X[i, :]
-		y_i = Y[i]
-		
+		y_i = Y[i]		
 		#sigmoidal
 		#h = 1/(1+np.exp(-(np.dot(np.transpose(w),x_i))))
 		#gradient += 2*(1-h)*(h)*(h-y_i)
@@ -81,7 +108,7 @@ for t in range(0, max_epochs):
 		epoch_index.append(t)
 		sse_testing_i.append(err)
 	sse.append(sse_i)
-
+"""
 print "Testing ... "
 #Load the testing data set
 X_file_testing = np.genfromtxt(filename, delimiter=',', skip_header=max_training+1)
@@ -101,22 +128,22 @@ Y = X_file_testing[:,0]
 for i in range(1,np.shape(X)[1]):
 	X[:,i] = (X[:, i]-np.mean(X[:, i]))/np.std(X[:, i])
 err = 0
-
-print np.shape(w), np.shape(X)
+#print np.shape(w), np.shape(X)
 #Find the accuracy and error from target and calculated output
 for i in range(0, N):
-	err += abs(Y[i] - np.dot(np.transpose(w),X[i,:]))/Y[i]
-print "Wieghts : ",w
+		h = np.zeros(max_hidden_nodes)
+		for k in range(0, max_hidden_nodes):
+			h[k] = sigmoidal(p[:,k],X[i,:])
+			o_i = np.dot(np.transpose(h), q)
+		#print Y[i], o_i
+		err += Y[i] - o_i
+
 print "Error: ",(err/N)*100,"%"
 epochs = [epoch_i+1 for epoch_i in range(0,max_epochs)]
 
 
 #Plot the SSE vs Epochs
-training_curve, = plt.plot(epochs, sse, label="training set")
-#plot the SSE vs epochs for every 10 epochs
-testing_curve, = plt.plot(epoch_index, sse_testing_i, label="testing set")
-
-plt.legend([training_curve, testing_curve],["training","testing"])
+plt.plot(epochs, sse, label="training set")
 plt.xlabel('Epochs')
 plt.ylabel('SSE')
 plt.title('BodyFat ANN Error:'+str((err/N)*100)+'%')
@@ -124,19 +151,3 @@ plt.savefig('Eta:'+str(eta)+'.png')
 
 print "Completed. SSE vs Epochs plot for the given Eta is saved in the project directory.\n"
 
-#remove this comment to get predicted values and calculated values
-plt.cla()
-
-actual_data_i = []
-predicted_i = []
-for i in range(0, N):
-	predicted_i.append(np.dot(np.transpose(w),X[i,:]))
-	actual_data_i.append(Y[i])
-n = len(predicted_i)
-predicted = plt.scatter([i for i in range(0,n)],predicted_i,c="r",alpha=0.5)
-actual = plt.scatter([i for i in range(0,n)],actual_data_i,c="b",alpha=0.5)
-plt.legend([predicted, actual],["predicted", "actual"])
-plt.xlabel('data row no.')
-plt.ylabel('Target')
-plt.title("Deviation")
-plt.savefig("target deviation for Eta:"+str(eta)+".png")
